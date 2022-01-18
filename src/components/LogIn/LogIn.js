27eb885679bfd320d4887cd3be5c37c7c04bde98
Ricardo <rr155914@gmail.com>
2022-01-17 22:38:52 -0600

    add basic html log in

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -3,9 +3,17 @@ import React from 'react'
 const LogIn = () => {
     return (
         <div>
-            <div className="login-title">Usuario</div>
-            <input type="text" className="username" label="Username"/>
-
+            <div className="header title">Welcome!</div>
+            <br />
+            <div className="login-name">Name</div>
+            <input type="text" className="username" placeholder=" First Name"/>
+            <input type="text" className="lastname" placeholder="Last Name"/>
+            <div className="email title">Email</div>
+            <input type="text" className="lastname" placeholder="user@...com"/>
+            <div className="email title">Phone Number</div>
+            <input type="text" className="lastname" placeholder="222-222-2222"/>
+            <br />
+            <button className="login btn">Submit</button>
         </div>
     )
 }

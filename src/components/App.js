575c4ae5775c4f5a2e45c0fb29d63fdd5ee1855b
Ricardo <rr155914@gmail.com>
2022-01-17 22:18:17 -0600

    Add input user

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import LogIn from './LogIn/LogIn'
+
+
+
+const App = () => {
+    return (
+        <div>
+            <LogIn />
+        </div>
+    )
+}
+
+export default App
+
+
---
+++ src/components/LogIn/LogIn.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LogIn = () => {
+    return (
+        <div>
+            <div className="login-title">Usuario</div>
+            <input type="text" className="username" label="Username"/>
+
+        </div>
+    )
+}
+
+export default LogIn
